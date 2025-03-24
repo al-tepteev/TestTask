65306b2a36d4a03b48c2b0969768527fefb24fcb
Aleksandr Tepteev <al_tepteev@vk.com>
2025-03-24 12:33:13 +0500

    Добавил pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-03-20',
   devtools: { enabled: true },
+
+  modules: ['@pinia/nuxt'],
   
   css: ['~/assets/style/tailwind.css'],
+
   vite: {
     plugins: [
       tailwindcss(),
