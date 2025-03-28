6ebf4317c608203c695b168b45f74bf745033415
Aleksandr Tepteev <al_tepteev@vk.com>
2025-03-28 14:15:11 +0500

    добавил trailingSlash

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  experimental: {
+    defaults: {
+      nuxtLink: {
+        trailingSlash: "append", // can be 'append' or 'remove'
+      },
+    },
+  },
+  
   compatibilityDate: '2025-03-24',
   devtools: { enabled: true },
 
